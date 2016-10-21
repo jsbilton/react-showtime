4516bb6cb65bd1a0bf3994ea94cdfa5f643c30a5
Jared Bilton <jsbilton@gmail.com>
2016-10-21 09:36:10 -0400

    cleaned up format

--- src/components/moviedb.js
+++ src/components/moviedb.js
@@ -1,12 +1,15 @@
 const h = require('react-hyperscript')
-
 const {Link} = require('react-router')
 const React = require('react')
 const xhr = require('xhr')
 
 const Moviedb = React.createClass({
     getInitialState: function() {
-        return {s: '', r: 'json', movies: []}
+        return {
+          s: '',
+          r: 'json',
+          movies: []
+        }
     },
     handleSubmit: function(e) {
         e.preventDefault()
@@ -25,15 +28,21 @@ const Moviedb = React.createClass({
         this.setState({s: e.target.value})
     },
     render: function() {
-        return (h('div.vh-100.cover.bg-left.bg-center-l.bg-black-80.ma0.pa0', {
+        return (
+          h('div.vh-100.cover.bg-left.bg-center-l.bg-black-80.ma0.pa0', {
             style: {
                 backgroundImage: 'url(http://images.fineartamerica.com/images-medium-large-5/movie-reels-marzena-grabczynska-lorenc.jpg)'
             }
-        }, [h('div.vh-100.bg-black-70', [
+        }, [
+          h('div.vh-100.bg-black-70', [
                 h('h1.fw1.f1.avenir.tc.white.pa2', 'Movies'),
                 h('form.pa2.tc', {
                     onSubmit: this.handleSubmit
-                }, [h('input.pa2.input-reset.ba-white.bg-transparent.hover-bg-white.hover-black.w-50', {onChange: this.handleChange})]),
+                }, [
+                  h('input.pa2.input-reset.ba-white.bg-transparent.hover-bg-white.hover-black.w-50', {
+                    onChange: this.handleChange}
+                  )]
+                ),
                 h(Link, {
                     to: '/',
                     className: 'link db mt2 pa2 tc black avenir white'
@@ -48,7 +57,8 @@ function movie(m) {
         to: `/moviedb/${m.imdbID}`,
         className: 'fl w-50 w-25-l link overflow-hidden',
         href: '#'
-    }, [h("div.grow.aspect-ratio--4x6.", {
+    }, [
+      h("div.grow.aspect-ratio--4x6.", {
             style: {
                 background: `url(${m.Poster}) no-repeat center center`,
                 backgroundSize: 'cover'
